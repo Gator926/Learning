4d4348b4d5586f295137883a8cf78cf663936909
gator <zhangpei529@sohu.com>
2021-07-06 18:03:05 +0800

    oop学习

--- oop/src/main/java/InstanceTest.java
+++ oop/src/main/java/InstanceTest.java
@@ -3,7 +3,16 @@
  *  1. 在java语言范畴中，将功能、结构封装到类中，通过类的实例化，来调用具体的功能结构
  *  2. 设计到java语言与前端、数据库交互时，有前后端的结构在java层面交互时，都提现为类、对象
  *
+ * 内存解析
+ *  1. 引用类型的变量，只课鞥存在两类值：null或地址值（含变量的类型）
  */
 public class InstanceTest {
+    public static void main(String[] args) {
+        Phone phone = new Phone();
+
+    }
+}
+
+class Phone{
 
 }
