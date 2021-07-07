cbf91caf92ce1afa0d79c568b63fdd61d0cdb94f
gator <zhangpei529@sohu.com>
2021-07-07 11:26:10 +0800

    oop学习

--- oop/src/main/java/ValueTransferTest.java
+++ oop/src/main/java/ValueTransferTest.java
@@ -3,7 +3,7 @@ import javax.xml.transform.Source;
 /**
  * 关于变量的赋值:
  *      如果变量是基本数据类型，此时赋值的是变量所保存的数据值
- *      如果变量是引用数据类型，此时赋值的是变量所爆粗的地址值
+ *      如果变量是引用数据类型，此时赋值的是变量所保存的地址值
  */
 public class ValueTransferTest {
     public static void main(String[] args) {
---
+++ oop/src/main/java/ValueTransferTest1.java
@@ -0,0 +1,10 @@
+/**
+ * 关于变量的赋值:
+ *      如果变量是基本数据类型，此时赋值的是变量所保存的数据值
+ *      如果变量是引用数据类型，此时赋值的是变量所爆粗的地址值
+ */
+public class ValueTransferTest1 {
+    public static void main(String[] args) {
+
+    }
+}
