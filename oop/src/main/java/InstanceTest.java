623ccfa08b3634ebca7ee339f038008db898bfed
gator <zhangpei529@sohu.com>
2021-07-07 10:09:34 +0800

    oop学习

--- oop/src/main/java/InstanceTest.java
+++ oop/src/main/java/InstanceTest.java
@@ -5,14 +5,55 @@
  *
  * 内存解析
  *  1. 引用类型的变量，只课鞥存在两类值：null或地址值（含变量的类型）
+ *
+ *  匿名对象的使用
+ *      理解：创建的对象，没有显示赋值一个变量名，即匿名对象
+ *      特征：匿名对象只能调用一次
+ *      使用：
+ *          phoneMall phoneMall = new phoneMall();
+ *          new phoneMall().show(new Phone());
  */
 public class InstanceTest {
     public static void main(String[] args) {
         Phone phone = new Phone();
+        System.out.println(phone);
 
+        phone.sendEmail();
+        phone.playGame();
+
+        System.out.println("------------");
+        // 匿名对象
+        new Phone().sendEmail();
+        new Phone().playGame();
+
+        new Phone().price = 1999;
+        new Phone().showPrice();
+
+        System.out.println("-------------");
+        phoneMall phoneMall = new phoneMall();
+        new phoneMall().show(new Phone());      // 匿名对象的使用
+    }
+}
+
+class phoneMall{
+    public void show(Phone phone){
+        phone.sendEmail();
+        phone.playGame();
     }
 }
 
 class Phone{
+    double price;
 
+    public void sendEmail(){
+        System.out.println("发送邮件");
+    }
+
+    public void playGame(){
+        System.out.println("玩游戏");
+    }
+
+    public void  showPrice(){
+        System.out.println("手机价格为: " + price);
+    }
 }
